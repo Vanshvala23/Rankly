118e0e16ff25fe1d7ae8aaa8911085f19f1049b8
VanshVala <102245923+Vanshvala23@users.noreply.github.com>
2026-09-22 19:36:54 +0530

    Update Code

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,4 +1,14 @@
 const serverless = require('serverless-http');
 const app = require('../../server');
 
-module.exports.handler = serverless(app);
+const handler = serverless(app);
+
+module.exports.handler = (event, context) => {
+  if (!event.path.startsWith('/api')) {
+    event.path = `/api${event.path}`;
+  }
+  if (event.rawUrl) {
+    event.rawUrl = event.rawUrl.replace('/.netlify/functions/api', '');
+  }
+  return handler(event, context);
+};
